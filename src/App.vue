dd5cf35586e641cd81386144d0a3144c1c513e35
Jamie Wang <jamie0828@163.com>
2022-09-01 10:20:06 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <!-- For render-style=read you should try to provide height of rapi-doc element as it needs to calculate scroll positions-->
+    <rapi-doc
+      spec-url="https://petstore.swagger.io/v2/swagger.json"
+      render-style="read"
+      style="height:100vh; width:100%"
+    ></rapi-doc>
+  </div>
+</template>
+
+<script>
+import "rapidoc";
+</script>
